07b6708fb55997cdca6d558436a7aeaded613921
Parash Gurung <unforgivingboy@gmail.com>
2017-02-12 19:46:51 +0545

    Fixed and prefectly working

--- Assig-5/face.C
+++ Assig-5/face.C
@@ -1,5 +1,7 @@
 //To input number and Accending using Function
 #include<stdio.h>
+int anum[10];
+int c=0;
 int display(int);
 
 int main(){
@@ -10,7 +12,6 @@ int main(){
  }
  printf("\n");
  
- printf("Now, this is printed using function : \n");
  for(a=0;a<10;a++){
 	display(n[a]);
 	
@@ -19,8 +20,29 @@ int main(){
  return 0;
 }
 
-int display(int x){
-	printf("%d ", x);
-
+int display(int b){
+	int x,y,temp;
+	anum[c]=b;
+	c++;
+	if(c==9){
+	
+		/*for(x=0;x<10;x++){
+			printf("%d", anum[x]);
+		}*/
+		for(x=0;x<10;x++){
+			for(y=x+1;y<10;y++){
+				if(anum[x]>anum[y]){
+					temp = anum[x];
+					anum[x] = anum[y];
+					anum[y] = temp;
+				}
+			}
+		}
+		printf("The sorted list is : \n");
+		for(x=0;x<10;x++){
+			printf("%d ", anum[x]);
+		}
+	}
+	
  return 0;
 }
