aafc6e25c6953e78cec939eb07716924cb82d37f
Parash Gurung <unforgivingboy@gmail.com>
2017-02-12 19:09:56 +0545

    New addition

---
+++ Assig-5/callarray.C
@@ -0,0 +1,26 @@
+//To input number and Print using Function
+#include<stdio.h>
+int display(int);
+
+int main(){
+ int a,n[10];
+ printf("Input any 10 numbers : \n");
+ for(a=0;a<10;a++){
+	scanf("%d", &n[a]);
+ }
+ printf("\n");
+ 
+ printf("Now, this is printed using function : \n");
+ for(a=0;a<10;a++){
+	display(n[a]);
+	
+ }
+ 
+ return 0;
+}
+
+int display(int x){
+	printf("%d ", x);
+
+ return 0;
+}
